4d97ab5006ddbbac166983d749de0af8fd3cf375
parthk5 <parth.khanna15@gmail.com>
2024-01-06 17:15:07 -0800

    Version 1.1.1

--- test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-print(f"Received: {sys.argv[0]}")
-if os.path.exists("arg.txt"):
-    os.remove('arg.txt')
-with open('arg.txt', 'w') as f:
-    f.write(f"Received: {sys.argv[1]}")
